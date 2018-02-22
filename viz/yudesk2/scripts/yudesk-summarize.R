97173d4380f744b28c55085bcbb51c8d33791b5a
William Denton <wtd@pobox.com>
2018-02-22 15:30:58 -0500

    YuDesk2 refactoring

--- viz/yudesk2/scripts/yudesk-summarize.R
+++ viz/yudesk2/scripts/yudesk-summarize.R
@@ -9,10 +9,10 @@ libstats_data_file  <- paste0(Sys.getenv("DASHYUL_DATA"), "/libstats/libstats.cs
 yudesk_summary_file <- paste0(Sys.getenv("DASHYUL_DATA"), "/yudesk/yudesk-summary.csv")
 
 l <- read_csv(libstats_data_file) %>% mutate(date = as.Date(timestamp, format="%m/%d/%Y %r"), month_name = month(date, label = TRUE))
-l$academic_year <- academic_year(l$date)
+l$ayear <- academic_year(l$date)
 ## l$library.name <- as.factor(l$library.name)
 
 ## Filter out this month's data (so there's nothing incomplete that looks strange), then summarize.
-yudesk_summary <- l %>% filter(date < floor_date(Sys.Date(), "month")) %>% group_by(question.type, question.format, time.spent, library.name, location.name, month_name, academic_year) %>% summarise(count = n())
+yudesk_summary <- l %>% filter(date < floor_date(Sys.Date(), "month")) %>% group_by(question.type, question.format, time.spent, library.name, location.name, month_name, ayear) %>% summarise(count = n())
 
 write_csv(yudesk_summary, yudesk_summary_file)
