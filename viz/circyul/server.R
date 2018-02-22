1fa96658d74a168940bce1a2201672f6c936d357
William Denton <wtd@pobox.com>
2018-02-22 11:23:11 -0500

    CircYUL tweaks

--- viz/circyul/server.R
+++ viz/circyul/server.R
@@ -8,12 +8,12 @@ library(yulr)
 
 ## circyul_data_dir <- paste0(Sys.getenv("DASHYUL_DATA"), "/circyul/")
 circyul_data_dir <- "/dashyul/data/circyul/"
-circyul_checkouts_file <- paste0(circyul_data_dir, "checkouts.csv.gz")
-circulated_item_details_file <- paste0(circyul_data_dir, "circulated_item_details.csv.gz")
+circyul_checkouts_file         <- paste0(circyul_data_dir, "checkouts.csv.gz")
+circulated_item_details_file   <- paste0(circyul_data_dir, "circulated_item_details.csv.gz")
 circulated_title_metadata_file <- paste0(circyul_data_dir, "circulated_title_metadata.csv.gz")
 
-checkouts <- read_csv(circyul_checkouts_file, col_types = "Dcc")
-circulated_item_details <- read_csv(circulated_item_details_file, col_types = "ccccccccDc")
+circyul_checkouts         <- read_csv(circyul_checkouts_file, col_types = "Dcc")
+circulated_item_details   <- read_csv(circulated_item_details_file, col_types = "ccccccccDc")
 circulated_title_metadata <- read_csv(circulated_title_metadata_file, col_types = "ccc")
 
 shinyServer(function(input, output, session) {
@@ -29,30 +29,22 @@ shinyServer(function(input, output, session) {
 
     record_item_history <- reactive({
         items <- circulated_item_details %>% filter(control_number == record_control_number())
-        checkouts %>% filter(item_barcode %in% items$item_barcode) %>% select(date, item_barcode) %>% mutate(date = as.Date(date))
+        circyul_checkouts %>% filter(item_barcode %in% items$item_barcode) %>% select(date, item_barcode) %>% mutate(date = as.Date(date))
     })
 
-    ## first_acquisition_date <- reactive({
-    ##     items <- circulated_item_details %>% filter(control_number == record_control_number())
-    ##     find_ayear(min(items$acq_date))
-    ## })
-
     output$title_information <- renderText({
-        ## Just the plain old 245, unformatted
         record_metadata <- circulated_title_metadata %>% filter(control_number == record_control_number())
-        record_metadata$title_author
+        readable_marc245(record_metadata$title_author)
     })
 
     output$circ_history_plot <- renderPlot({
         checkouts_by_ayear <- record_item_history() %>% mutate(ayear = academic_year_as_year(date)) ## %>% count(ayear)
-
         ## ggplot(checkouts_by_ayear, aes(x = ayear, y = n)) + geom_col() + labs(title = paste("Checkouts"), x = "Academic year", y = "") + scale_x_date(date_labels = "%Y")
-        ggplot(checkouts_by_ayear, aes(x = ayear)) + geom_bar(width = 300) + labs(title = paste("Circs per year"), x = "Academic year", y = "") ## + scale_x_date(date_labels = "%Y")
-        ## ggplot(checkouts_by_ayear, aes(ayear)) + geom_histogram(bins = 10) ##, boundary = 0.1, closed = "left")
+        ggplot(checkouts_by_ayear, aes(x = ayear)) + geom_bar(width = 300) + labs(title = paste("Circs per academic year"), x = "", y = "") + scale_y_continuous(breaks = pretty_breaks())
     })
 
     output$item_history_table <- renderTable({
-        item_circ_summary <- record_item_history() %>% group_by(item_barcode) %>% summarise(circs = n())
+        item_circ_summary <- record_item_history() %>% count(item_barcode)
         item_details <- circulated_item_details %>% filter(item_barcode %in% item_circ_summary$item_barcode) %>% select(item_barcode, item_type, acq_date)
         last_circs <- record_item_history() %>% group_by(item_barcode) %>% mutate(last_circ = max(date)) %>% select(item_barcode, last_circ) %>% distinct
         item_history_table <- merge(item_circ_summary, item_details) %>% merge(last_circs) %>% mutate(acq_date = as.character(acq_date), last_circ = as.character(last_circ))
--- viz/circyul/ui.R
+++ viz/circyul/ui.R
@@ -4,12 +4,13 @@ shinyUI(
 
     fluidPage(
 
-        titlePanel("CircYUL: YUL circulation history"),
+        titlePanel("CircYUL"),
 
         sidebarLayout(
 
             sidebarPanel(
 
+                tags$p("CircYUL shows the total annual circulations of all items of a given bibliographic record of something in our circulating collection."),
                 textInput("raw_control_number", label = "Control number", value = "https://www.library.yorku.ca/find/Record/2184579"),
                 tags$p("The control number is the number at the end of the URL in VuFind. Enter it alone, or just paste in the entire URL."),
                 tags$p("Circulation records cover A2006-A2016."),
