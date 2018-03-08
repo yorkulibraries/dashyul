27659db043bff99c55923c1bb19d5fd3c6e0266d
William Denton <wtd@pobox.com>
2018-03-08 12:28:48 -0500

    Tweak EZP daily count script, for cronning

--- viz/dashboard/scripts/count-ezproxy-users.sh
+++ viz/dashboard/scripts/count-ezproxy-users.sh
@@ -6,6 +6,11 @@ DASHBOARD_DATA=${DASHYUL_DATA}/viz/dashboard
 YESTERDAY="${DASHBOARD_DATA}/ezp-users-yesterday.txt"
 TODAY="${DASHBOARD_DATA}/ezp-users-today.txt"
 
+echo "------"
+echo "Started: `date`"
+
 mv $TODAY $YESTERDAY
 
 cut -d, -f2 ${EZP_CURRENT_DATA}/*detailed* | sort | uniq | wc -l > $TODAY
+
+echo "Finished: `date`"
