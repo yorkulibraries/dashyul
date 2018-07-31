b0b2b54ac56894ab28271e9ba11049cca5767e89
William Denton <wtd@pobox.com>
2018-07-31 15:50:45 -0400

    Save catalogue-item-details CSV as RDS tibble

---
+++ sources/symphony/scripts/extract-catalogue-item-details.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+
+## Convert a large CSV file into a structured R data frame,
+## suitable for easy loading and reuse.
+##
+## All this does is load in catalogue-YYYYMMDD-item-details.csv,
+## define the column types (character, integer, etc.) and then
+## save it as an RDS file.
+
+"usage: extract-catalogue-item-details.R --dump-file <prefix>
+
+options:
+ --dump-file <prefix>        Basename prefix to use
+" -> doc
+
+library(docopt)
+library(readr)
+
+opts <- docopt(doc)
+
+dump_file <- opts["dump-file"]
+
+catalogue_data_dir   <- paste0(Sys.getenv("DASHYUL_DATA"), "/symphony/catalogue/")
+
+item_details_csv <- paste0(catalogue_data_dir, dump_file, "-item-details.csv")
+item_details_rds <- paste0(catalogue_data_dir, dump_file, "-item-details.rds")
+
+item_details <- read_csv(item_details_csv, col_types = "cccciiDDDiiccciccicccDcc")
+saveRDS(item_details, file = item_details_rds)
--- sources/symphony/scripts/process-catalogue-dump.sh
+++ sources/symphony/scripts/process-catalogue-dump.sh
@@ -3,33 +3,36 @@
 # Turn the huge monthly catalogue MARC dump into more usable files.
 
 echo "------"
-echo "Started: `date`"
+echo "Started: $(date)"
 
 SYMPHONY_SCRIPTS=${DASHYUL_HOME}/sources/symphony/scripts
 SYMPHONY_CATALOGUE_DATA=${DASHYUL_DATA}/symphony/catalogue
 
 MARC_DUMP=$1
-if [ -z $MARC_DUMP ]
+if [ -z "$MARC_DUMP" ]
 then
-    MARC_DUMP=`ls -rt /sirsilogs/catalogue*mrc | tail -1`
+    MARC_DUMP=$(ls -rt /sirsilogs/catalogue*mrc | tail -1)
 fi
-DUMP_FILE=`basename -s .mrc $MARC_DUMP`
+DUMP_FILE=$(basename -s .mrc "$MARC_DUMP")
 
 cd ${SYMPHONY_CATALOGUE_DATA}
 
 echo -n "$DUMP_FILE:  item details (~ 25 mins) ... "
 ${SYMPHONY_SCRIPTS}/extract-catalogue-item-details.rb $MARC_DUMP > ${DUMP_FILE}-item-details.csv
+${SYMPHONY_SCRIPTS}/extract-catalogue-item-details.R --dump-file ${DUMP_FILE}
 
 echo -n "title metadata ... (~ 20 mins) ... "
 ${SYMPHONY_SCRIPTS}/extract-catalogue-title-metadata.rb $MARC_DUMP > ${DUMP_FILE}-title-metadata.csv
 
 echo -n "to text ... (~ 1 mins) "
-yaz-marcdump $MARC_DUMP > ${DUMP_FILE}.txt
+yaz-marcdump "$MARC_DUMP" > "${DUMP_FILE}.txt"
 
 echo -n "linking ..."
 rm -f catalogue-current-item-details.csv
+rm -f catalogue-current-item-details.rds
 rm -f catalogue-current-title-metadata.csv
 ln -s ${DUMP_FILE}-item-details.csv catalogue-current-item-details.csv
+ln -s ${DUMP_FILE}-item-details.rds catalogue-current-item-details.rds
 ln -s ${DUMP_FILE}-title-metadata.csv catalogue-current-title-metadata.csv
 
-echo "Finished: `date`"
+echo "Finished: $(date)"
