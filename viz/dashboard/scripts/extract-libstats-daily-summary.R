658c99d270333e9ac2b457f7c6f0cd79a747ac32
William Denton <wtd@pobox.com>
2018-09-11 12:44:39 -0400

    Fix LibStats daily summary rollover

--- viz/dashboard/scripts/extract-libstats-daily-summary.R
+++ viz/dashboard/scripts/extract-libstats-daily-summary.R
@@ -12,14 +12,21 @@ libstats_data_file  <- paste0(Sys.getenv("DASHYUL_DATA"), "/libstats/libstats.cs
 
 libstats_daily_summary_file <- paste0(Sys.getenv("DASHYUL_DATA"), "/viz/dashboard/libstats-daily-summary.csv")
 
-l <- read_csv(libstats_data_file) %>% mutate(date = as.Date(timestamp, format="%m/%d/%Y %r"), month_name = month(date, label = TRUE))
+## Summary stats shown numbers for the current academic year.
+## Could get a little funny on 01 September, but so be it.
+current_academic_year <- academic_year(Sys.Date())
+
+l <- read_csv(libstats_data_file) %>%
+    mutate(date = as.Date(timestamp, format = "%m/%d/%Y %r"),
+           month_name = month(date, label = TRUE))
 l$academic_year <- academic_year(l$date)
 l$library.name <- as.factor(l$library.name)
 
-## Filter out this month's data (so there's nothing incomplete that looks strange), then summarize.
-## summary <- l %>% filter(academic_year == 2017) %>% filter(date < floor_date(Sys.Date(), "month")) %>% filter(question.type != "LCO") %>% group_by(library.name, question.type) %>% summarise(count = n())
-
-summary <- l %>% filter(academic_year == 2017) %>% filter(question.type != "LCO") %>% group_by(library.name, question.type) %>% summarise(count = n())
+summary <- l %>%
+    filter(academic_year == current_academic_year) %>%
+    filter(question.type != "LCO") %>%
+    group_by(library.name, question.type) %>%
+    summarise(count = n())
 
 write_csv(summary, libstats_daily_summary_file)
 
