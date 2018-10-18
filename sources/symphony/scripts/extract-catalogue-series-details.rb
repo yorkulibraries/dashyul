d7f18e5ee86e14f3a49d13587cba1f6bac205f0f
William Denton <wtd@pobox.com>
2018-10-18 17:26:51 -0400

    Bug fix on series data extract script

--- sources/symphony/scripts/extract-catalogue-series-details.rb
+++ sources/symphony/scripts/extract-catalogue-series-details.rb
@@ -12,24 +12,37 @@ end
 
 reader = MARC::Reader.new(marcfile, external_encoding: "UTF-8", invalid: :replace)
 
-puts %w[control_number publisher series_title series_volume].to_csv
+puts %w[control_number publisher series_title series_volume url].to_csv
 
 for record in reader
   begin
     sirsi_number = record.fields("035").find { |c| /Sirsi/.match(c["a"]) }
     control_number = sirsi_number["a"].gsub(/.* /, "")
+
+    # Series info
     next unless record["490"] || record["440"]
     series_title = ""
     series_volume = ""
     if record["490"]
       series_title  = record["490"]["a"].to_s
-      series_volume = record["490"]["v"].to_s || ""
+      series_volume = record["490"]["v"].to_s
     else
       series_title  = record["440"]["a"].to_s
-      series_volume = record["440"]["v"].to_s || ""
+      series_volume = record["440"]["v"].to_s
     end
-    publisher = record["260"]["b"].to_s
-    puts [control_number, publisher, series_title, series_volume].to_csv
+
+    # URL
+    url = record["856"] ? record["856"]["u"].to_s : ""
+
+    # Publisher
+    publisher = ""
+    if record["260"]
+      publisher = record["260"]["b"].to_s
+    elsif record["264"]
+      publisher = record["264"]["b"].to_s
+    end
+
+    puts [control_number, publisher, series_title, series_volume, url].to_csv
   rescue StandardError => e
     STDERR.puts "Failed on #{control_number}: #{e}"
   end
