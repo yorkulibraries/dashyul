30007f41952b65b660f88c39f497a32e184a86ee
William Denton <wtd@pobox.com>
2021-09-02 13:51:18 -0400

    YuDesk2: more fiddling

--- viz/yudesk2/scripts/prepare-yudesk.R
+++ viz/yudesk2/scripts/prepare-yudesk.R
@@ -5,10 +5,12 @@ suppressMessages(library(lubridate))
 library(yulr)
 
 libstats_summary <- paste0(Sys.getenv("DASHYUL_DATA"), "/libstats/summary.rds")
-yudesk_summary <- paste0(Sys.getenv("DASHYUL_DATA"), "/viz/yudesk/yudesk-summary.csv")
+yudesk_summary <- paste0(Sys.getenv("DASHYUL_DATA"), "/viz/yudesk/yudesk-summary")
 
-readRDS(libstats_summary) %>%
+y <- readRDS(libstats_summary) %>%
     ## Filter out this month's data (so there's nothing incomplete that looks strange), then summarize.
     filter(date < floor_date(Sys.Date(), "month")) %>%
-    count(question.type, question.format, time_spent, library.name, location.name, month_name, ayear) %>%
-    write_csv(yudesk_summary)
+    count(question.type, question.format, time_spent, library.name, location.name, month_name, ayear)
+
+write_csv(y, paste0(yudesk_summary, ".csv"))
+saveRDS(y, paste0(yudesk_summary, ".rds"))
