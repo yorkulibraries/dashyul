c350e27bc58830ad36b45d6cb64d5972e619fb52
William Denton <wtd@pobox.com>
2021-09-02 13:38:42 -0400

    YuDesk2: updated scripts

--- viz/yudesk2/scripts/prepare-yudesk.R
+++ viz/yudesk2/scripts/prepare-yudesk.R
@@ -1,19 +1,14 @@
 #!/usr/bin/env Rscript
 
-library(tidyverse)
-library(lubridate)
+suppressMessages(library(tidyverse))
+suppressMessages(library(lubridate))
 library(yulr)
 
-libstats_data_file  <- paste0(Sys.getenv("DASHYUL_DATA"), "/libstats/libstats.csv")
-yudesk_summary_file <- paste0(Sys.getenv("DASHYUL_DATA"), "/viz/yudesk/yudesk-summary.csv")
+libstats_summary <- paste0(Sys.getenv("DASHYUL_DATA"), "/libstats/summary.rds")
+yudesk_summary <- paste0(Sys.getenv("DASHYUL_DATA"), "/viz/yudesk/yudesk-summary.csv")
 
-l <- read_csv(libstats_data_file) %>% mutate(date = as.Date(timestamp, format="%m/%d/%Y %r"), month_name = month(date, label = TRUE))
-l$ayear <- academic_year(l$date)
-
-## Filter out this month's data (so there's nothing incomplete that looks strange), then summarize.
-yudesk_summary <- l %>%
+readRDS(libstats_summary) %>%
+    ## Filter out this month's data (so there's nothing incomplete that looks strange), then summarize.
     filter(date < floor_date(Sys.Date(), "month")) %>%
-    group_by(question.type, question.format, time.spent, library.name, location.name, month_name, ayear) %>%
-    summarise(count = n())
-
-write_csv(yudesk_summary, yudesk_summary_file)
+    count(question.type, question.format, time_spent, library.name, location.name, month_name, ayear) %>%
+    write_csv(yudesk_summary)
